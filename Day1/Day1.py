a67a6f45ae04db18a5749c7156fd4c4e714db47f
PoloClebicar <151094973+PoloClebicar@users.noreply.github.com>
2023-12-12 23:32:42 +0000

    finish day 1 AoC 2023

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,12 +1,31 @@
-def main():
+
+def main():   
+    resultado = 0
     file = open("/workspaces/AoC_2023/Day1/list.txt", "r")
     
-    fileList = file.readlines()
+    #fileList = file.readlines()
+   
+    for lines in file:
+        resultado += find_numbers(lines)
     
-    i = 0
-    for files in fileList:
-        print(fileList[i])    #Have the breaking down of the str happen here
-        i = i + 1
+    print(resultado)
+    
+    
+
+def find_numbers(lines):
+    
+    numberList = []
+    for letters in lines:
+        if letters.isdigit():
+            numberList.append(letters)
+
+            if len(numberList) == 1:
+                sum = numberList[0] + numberList[0]
+            else:
+                sum = numberList[0] + numberList[len(numberList) - 1]
+    
+    return int(sum)
+
     
 
 main()
