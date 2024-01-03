ab7dbdead72e1f6a5818e2f99975465ceb56a099
PoloClebicar <clebicarpaulo@gmail.com>
2024-01-03 19:03:04 -0300

    Working on Day 5 Part 2

--- day5/part2/unfinished.py
+++ day5/part2/unfinished.py
@@ -36,48 +36,65 @@ def checkSmallestNumber(seedList):
 
 
 def compareList(seedList, mapList):
+
+    '''
+    TODO: Parsing the numbers correctly, but making a mistake in conversion is some point
+    Follow the conversions to find the erros
+    '''
+    print("Old Seed List : ", seedList)
     bufferList = []
     newSeedList = []
-    for i in range(int((len(seedList)/2))):
-       bufferList.append(seedList[i * 2])
-       bufferList.append(seedList[(i * 2) + 1])
-       print(bufferList)
+    #Naming the variables for the seed
+    for i in range(int(((len(seedList))/2))):
+        seedRangeStart = int(seedList[i *2])
+        seedRangeEnd = int(seedList[i *2]) + int(seedList[(i*2) + 1])
+        print("Seeds : ", seedRangeStart, seedRangeEnd)
 
+        for mapLists in mapList:
+            print("Maps :" ,mapLists)
+            #Naming the variables for map
+            mapRangeStart = int(mapLists[1])
+            mapRangeEnd = int(mapLists[1]) + int(mapLists[2])
+            mapKey = int(mapLists[1]) - int(mapLists[0])
 
-       #if Seedlist Start in within range of map list, and if SeedList end is withing range also
-       for maps in mapList:
-           
+            #Check if the seeds and maps intersect
 
-           '''
-           Figure out hwo to create a new list with the correct ranges #TODO
-           '''
-           #Check if seedrange start is within maprange
-           if int(maps[1]) <= int(bufferList[0]) <= int(maps[1]) + int(maps[2]):
-                #Check if the end of the range is within maprange
-                if int(maps[1]) <= int(bufferList[0]) + int(bufferList[1]) <= int(maps[1]) + int(maps[2]):
-                    print("Seed withing Range", maps)
-                else:
-                    print("Only Start of Seed within range", maps)
+            if mapRangeStart <= seedRangeStart <= mapRangeEnd and mapRangeStart <= seedRangeEnd <= mapRangeEnd:
+                #Seed fully inside map range
+                #Cover both Start and End of Seed Range converted
+                #We remove the Seed Range start from the end to create a Range, and not a end position
+                bufferList = [seedRangeStart + mapKey,(seedRangeEnd + mapKey) - seedRangeStart]
+                
+            elif seedRangeStart <= mapRangeStart <= seedRangeEnd and seedRangeStart <= mapRangeEnd <= seedRangeEnd:
+                #Map range fully withing Seeds
+                #Create a list with SeedStart -> MapStart -1, 
+                #another range from MapStart -> MapEnd, 
+                #another from Mapend + 1 -> Seed End
+                bufferList = [seedRangeStart, ((mapRangeStart - 1) - seedRangeStart),
+                              (mapRangeStart + mapKey), (mapRangeEnd + mapKey),
+                              (mapRangeEnd + 1), (seedRangeEnd)]
+            elif mapRangeStart <= seedRangeStart <= mapRangeEnd and not (mapRangeStart <= seedRangeStart <= mapRangeEnd):
+                #Seed start within but seed end out
+                #Create a range with Start converted -> Maprange end, maprangend +1 -> normal Seed end 
+                bufferList = [(seedRangeStart + mapKey), (mapRangeEnd - seedRangeStart),
+                              (mapRangeEnd + 1), (seedRangeEnd)]
+            elif not(mapRangeStart <= seedRangeStart <= mapRangeEnd) and mapRangeStart <= seedRangeStart <= mapRangeEnd:
+                #Seed start out but seed end within
+                #Create a range SeedStartNormal -> SeedStart -1,
+                #SeedStart Converted ->imapRangeEndCoverted,
+                bufferList = [(seedRangeStart), (mapRangeStart - 1),
+                              (mapRangeStart + mapKey),(seedRangeEnd + mapKey - seedRangeStart)]
+            else:
+                #The ranges do not overlap
+                bufferList = [(seedRangeStart), (seedRangeEnd - seedRangeStart)]
 
-           else:
-               print("Seeds Not in range",maps)
+        for itens in bufferList:
+            newSeedList.append(itens)
 
-          #Check it the range is within seed 
-           if int(bufferList[0]) <= int(maps[1]) <= int(bufferList[0]) + int(bufferList[1]):
-                #Check if the end of the maprange is within seedrange
-                if int(bufferList[0]) <= int(map[1]) + int(maps[2]) <= int(bufferList[0]) + int(bufferList[1]):
-                    print("Map withing SeedRange", maps)
-                else:
-                    print("Only Start of Map within range", maps)
+        bufferList.clear() 
 
-           else:
-               print("Map Not in range",maps)
-
-
-       bufferList.clear()
-
-    
-    print(mapList) 
+    print("New Seed List : " ,newSeedList)
+    return newSeedList 
 
         
 def treatCoordinates(rawCoord):
